e132978806e59b8b5ff08571fc2eb6a12f9bdacb
QuentinGolinveau <quentingolinveau@gmail.com>
2022-06-03 15:14:48 +0200

    Anim GSAP

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,38 @@
-console.info('Hello world');
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".balle", { 
+    duration: 1.5, 
+    ease: "bounce.out",
+    x: -2500,
+    scrollTrigger:{
+        trigger:".section--marque",
+        start:"center bottom",
+        markers: true,
+    },
+});
+
+gsap.from(".balle--v2",{
+    duration: 1, 
+    ease: "bounce.out",
+    y: -400,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".section--choix",
+        start:"top top",
+        markers: true,
+    },
+});
+
+gsap.from(".section--choix .fond--image", {
+    opacity: 0.2,
+    duration: 0.5,
+    x: -500,
+    scrollTrigger:{
+        trigger:".section--choix",
+        start:"center bottom",
+        markers: true,
+    },
+});
